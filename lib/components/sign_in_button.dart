547d1a5164586684d6386aae39dad3be7a6225ea
Anoop Jose <anoop@flutterly.co.uk>
2022-10-12 00:46:06 +0100

    Started working on the app bar

---
+++ lib/components/sign_in_button.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: public_member_api_docs
+
+import 'package:flutter/material.dart';
+
+class SignInButton extends StatelessWidget {
+  const SignInButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(onPressed: () {}, child: const Text('Sign in'));
+  }
+}
